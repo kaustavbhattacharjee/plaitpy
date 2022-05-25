4e93438183b8353a6057ac773da84f0003339421
Kaustav Bhattacharjee <kaustavbhattacharjee@users.noreply.github.com>
2022-05-25 14:39:19 -0400

    fix: adjusted folders

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import plaitpy
 import pandas as pd
-t = plaitpy.Template("templates/timestamp/uniform1.yml")
+t = plaitpy.Template("templates/kb/uniform1.yml")
 my_pd = t.gen_records(100)
 timeseries_df = pd.concat([pd.DataFrame(d, index=[1]) for d in my_pd]).reset_index(drop=True)
 print(timeseries_df)
