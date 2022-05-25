ec0190607184d794053bf54d49d87e5166ad7b77
Kaustav Bhattacharjee <kaustavbhattacharjee@users.noreply.github.com>
2022-05-25 18:09:41 -0400

    feature: added sample county records

---
+++ dataset_generator.py
@@ -0,0 +1,16 @@
+import plaitpy
+import pandas as pd
+
+# Mobile Clinic records
+t1 = plaitpy.Template("templates/kb/mobile_clinic.yml")
+t1_output = t1.gen_records(100)
+mobile_clinic_records = pd.concat([pd.DataFrame(d, index=[1]) for d in t1_output]).reset_index(drop=True)
+mobile_clinic_records.to_csv("datasets/mobile_clinic_records.csv",index=False)
+print(mobile_clinic_records)
+
+# County records
+t2 = plaitpy.Template("templates/kb/county_records.yml")
+t2_output = t2.gen_records(100)
+county_records = pd.concat([pd.DataFrame(d, index=[1]) for d in t2_output]).reset_index(drop=True)
+county_records.to_csv("datasets/county_records.csv",index=False)
+print(county_records)
