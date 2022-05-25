6297d3aeb2544865ace11db9fff2739c9ee565fa
Kaustav Bhattacharjee <kaustavbhattacharjee@users.noreply.github.com>
2022-05-25 17:37:13 -0400

    feature: made format for mobile clinic records

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import plaitpy
 import pandas as pd
-t = plaitpy.Template("templates/kb/uniform1.yml")
+t = plaitpy.Template("templates/kb/mobile_clinic.yml")
 my_pd = t.gen_records(100)
+#print(my_pd)
 timeseries_df = pd.concat([pd.DataFrame(d, index=[1]) for d in my_pd]).reset_index(drop=True)
 print(timeseries_df)
 
