e57f27e093e4cc291f4e916b33c83fdcb20772c6
Kaustav Bhattacharjee <kaustavbhattacharjee@users.noreply.github.com>
2022-05-25 01:18:00 +0000

    test5

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import pandas as pd
 t = plaitpy.Template("templates/timestamp/uniform1.yml")
 print(t.gen_record(2))
 my_pd = pd.DataFrame(t.gen_record(2))
-print(pd)
+print(my_pd)
 #print(t.gen_records(10))
